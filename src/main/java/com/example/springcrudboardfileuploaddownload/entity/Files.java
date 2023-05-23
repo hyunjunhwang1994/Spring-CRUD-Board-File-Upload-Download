1311df196033d3e540ba9596b1164174f79a8cf6
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-05-23 10:35:01 +0900

    feat: Files Entity 추가
    파일저장 용도의 Files Entity 추가

---
+++ src/main/java/com/example/springcrudboardfileuploaddownload/entity/Files.java
@@ -0,0 +1,16 @@
+package com.example.springcrudboardfileuploaddownload.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Files {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
