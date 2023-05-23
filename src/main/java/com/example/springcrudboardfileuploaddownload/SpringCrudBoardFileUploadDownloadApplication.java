c54643329c0958aca1c04672e12d6a3f0a971d1d
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-05-23 10:05:43 +0900

    Initial commit

---
+++ src/main/java/com/example/springcrudboardfileuploaddownload/SpringCrudBoardFileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudboardfileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudBoardFileUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudBoardFileUploadDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcrudboardfileuploaddownload/SpringCrudBoardFileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrudboardfileuploaddownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudBoardFileUploadDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
